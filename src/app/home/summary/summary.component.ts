bcb1ac95bdba1d23846e266fdcbeba32b8adc078
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-08-24 12:17:52 +0200

    implement summary component

--- src/app/home/summary/summary.component.ts
+++ src/app/home/summary/summary.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FirebaseAuthenticationService } from '../../services/firebase-authentication/firebase-authentication.service';
 
 @Component({
   selector: 'app-summary',
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
+  public firebaseAuthenticationService: FirebaseAuthenticationService = inject(FirebaseAuthenticationService);
 
+  public getGreeting(): string {
+    const now = new Date();
+    const hours = now.getHours();
+    if (hours >= 0 && hours < 12) {
+      return 'Good Morning';
+    } else if (hours >= 12 && hours < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  }
 }
