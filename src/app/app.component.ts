a80fd258522081fdc0618614075294bc6d39f9b5
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-10-01 13:27:13 +0200

    Made the authentication and summary components responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeService } from './services/home/home.service';
@@ -11,5 +11,27 @@ import { HomeService } from './services/home/home.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public isLandscape: boolean = false;
+  public isLargeEnough: boolean = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkOrientation();
+  }
+
   public homeService: HomeService = inject(HomeService);
+
+  public ngOnInit(): void {
+    this.checkOrientation();
+  }
+
+  private checkOrientation(): void {
+    this.isLandscape = window.matchMedia('(orientation: landscape)').matches;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    this.isLargeEnough = width >= 600 && height >= 600; 
+    if(this.isLargeEnough) {
+      this.isLandscape = false;
+    }
+  }
 }
